8119502831318c86bdc06fea5f92cccab959e119
tetu137 <liuyongjiesb@163.com>
2022-08-29 11:19:24 +0800

    home首页

--- main.js
+++ main.js
@@ -2,6 +2,27 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+import {$http} from "@escook/request-miniprogram"
+uni.$http=$http
+// 配置请求根路径
+$http.baseUrl="https://api-ugo-web.itheima.net"
+// 请求拦截器
+$http.beforeRequest=function(options){
+  uni.showLoading({
+    title:"数据加载中....."
+  })
+}
+// 响应拦截器
+$http.afterRequest=function(){
+  uni.hideLoading()
+}
+uni.$showMsg=function(title="数据加载失败",duration=1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:"none"
+  })
+}
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
